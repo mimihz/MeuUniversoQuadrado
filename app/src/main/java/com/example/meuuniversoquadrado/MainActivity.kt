bfdf4848d929f57140c877d00ee62b345017f19f
mimihz <michelleluiza0902@gmail.com>
2023-04-27 17:17:13 -0300

    arrumando

---
+++ app/src/main/java/com/example/meuuniversoquadrado/MainActivity.kt
@@ -0,0 +1,69 @@
+package com.example.meuuniversoquadrado
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.VerticalAlignmentLine
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.meuuniversoquadrado.ui.theme.MeuUniversoQuadradoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MeuUniversoQuadradoTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                     MeuMundoQuadrado()
+                }
+            }
+        }
+    }
+}
+@Preview
+@Composable
+fun MeuMundoQuadrado(){
+    var resultado = 1
+     Column (
+         modifier = Modifier.fillMaxSize(),
+         verticalArrangement = Arrangement.Center,
+         horizontalAlignment = Alignment.CenterHorizontally
+      ) {
+          Image(
+              painter = painterResource(id = R.drawable.dice_1) ,
+              contentDescription =null,
+          )
+
+         Button(
+             onClick = {
+             resultado =(1..6).random()
+             }
+         ) {
+             Text(
+                 text = "Girar"
+             )
+         }
+
+
+     }
+
+
+
+
+}
